9fe8b06f06978b4afa9b320909f9690227848cf5
MD <amdadullah@tended.co.uk>
2021-02-13 04:53:18 +0000

    Minor-fixes

--- App/src/App.js
+++ App/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Hello   World</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
